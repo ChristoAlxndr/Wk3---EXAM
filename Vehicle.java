88629422bccab5f204bffc52b44a190e33fb754c
Christopher Alexander <ChristoAlxndr@Gmail.com>
2016-08-17 09:04:17 -0400

    Exam Submission 08/17/2016 @ 09:04

---
+++ Vehicle.java
@@ -0,0 +1,20 @@
+package com.exam;
+
+public class Vehicle {
+
+	public int wheels = 0;
+	public int windows = 0;
+	public int engine = 0;
+	public boolean running = false;
+	public boolean traveling = false;
+	
+	public boolean turnOn() {
+		return false;
+		
+	}
+	
+	public boolean inMotion() {
+		return false;
+	}
+	
+}
---
+++ VehicleA.java
@@ -0,0 +1,31 @@
+package com.exam;
+
+public class VehicleA extends Vehicle {
+	
+	Vehicle forrester = new Vehicle();
+	
+	public int wheels = 4;
+	public int windows = 6;
+	public int engine = 1;
+	public boolean running = true;
+	public boolean traveling = true;
+	
+	private String color = "green";
+	private int headlights = 4;
+	private int windshieldWipers = 3;
+	private String seatColor = "grey";
+	boolean radioOn = true;
+	
+	public VehicleA() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	forrester.turnON(boolean);
+	return true;
+	}
+
+	forrester.inMotion(boolean);
+		return true;
+
+}
